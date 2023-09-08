5a67ff0f64127a25d2b1f199618b46217cf4ed9f
mike <miguelbodio@gmail.com>
2023-09-09 00:18:07 +0100

    add responsive caroussel in testimonials section

--- src/Components/Caroussel.tsx
+++ src/Components/Caroussel.tsx
@@ -4,7 +4,7 @@ import {CarousselItem} from './index'
 import { testimonials } from '../Constants'
 import { AiOutlineArrowLeft,AiOutlineArrowRight} from 'react-icons/ai'
 import { RxDotFilled } from 'react-icons/rx'
-import { slide1,slide2,slide3 } from "../assets"
+
 
 
 const Caroussel = () => {
@@ -18,37 +18,7 @@ const Caroussel = () => {
         setActiveIndex(newIndex)
     }
 
-    const items = [
-        {
-            id:1,
-            name:'Leida',
-            work:'Baker',
-            content :"Lorem ipsum dolor sit amet consectetur. A in odio sit convallis diam rhoncus nunc. Fermentum facilisis tortor suscipit viverra neque elit urna egestas sed. Malesuada eget dolor egestas amet. Elementum mus at scelerisque ipsum vel nisl. Malesuada volutpat cursus pharetra felis rhoncus ",
-            img: slide1,
-        },
-        {
-            id:2,
-            name:'Leida',
-            work:'Baker',
-            content :"Lorem ipsum dolor sit amet consectetur. A in odio sit convallis diam rhoncus nunc. Fermentum facilisis tortor suscipit viverra neque elit urna egestas sed. Malesuada eget dolor egestas amet. Elementum mus at scelerisque ipsum vel nisl. Malesuada volutpat cursus pharetra felis rhoncus ",
-            img: slide2,
-        },
-        {
-            id:3,
-            name:'Leida',
-            work:'Baker',
-            content :"Lorem ipsum dolor sit amet consectetur. A in odio sit convallis diam rhoncus nunc. Fermentum facilisis tortor suscipit viverra neque elit urna egestas sed. Malesuada eget dolor egestas amet. Elementum mus at scelerisque ipsum vel nisl. Malesuada volutpat cursus pharetra felis rhoncus ",
-            img: slide3,
-        },
-        {
-            id:4,
-            name:'Leida',
-            work:'Baker',
-            content :"Lorem ipsum dolor sit amet consectetur. A in odio sit convallis diam rhoncus nunc. Fermentum facilisis tortor suscipit viverra neque elit urna egestas sed. Malesuada eget dolor egestas amet. Elementum mus at scelerisque ipsum vel nisl. Malesuada volutpat cursus pharetra felis rhoncus ",
-            img: slide2,
-        },
-    ]
-
+ 
   return (
     <div className='tesimonials-section w-full flex items-center justify-center' id='testimonials'>
         <div className='container relative w-full max-w-[1440px] px-4 py-8 flex flex-col justify-center items-center'>
@@ -56,29 +26,29 @@ const Caroussel = () => {
                 <h1 className='title  text-2xl font-bold lg:text-4xl'>Testimonials</h1>
                 <h1 className=' underTitle absolute text-5xl font-bold left-1/2 text-blue-900 -top-1/2 -z-10 md:text-7xl'>5</h1>
             </div>
-            <div className="caroussel overflow-hidden w-full flex flex-col justify-center lg:w-[700px]">
+            <div className="caroussel mt-5 overflow-hidden w-full flex flex-col justify-center lg:w-[100%]">
                 <div className="inner whitespace-nowrap transition-all duration-500" style={{transform : `translate(-${activeIndex *100}%)`}}>
-                    {items.map((item) => {
+                    {testimonials.map((item) => {
                     return <CarousselItem item={item}/> })}
                 </div>
                 <div className='caroussel-controls my-4 w-full relative flex flex-row justify-between items-center'>
-                    <div className=' cursor-pointer px-8 py-2 border-2 transition-all duration-300'>
+                    <div className='hidden cursor-pointer px-8 py-1 border-[1px] border-black transition-all duration-300 md:block hover:border-yellow-500 hover:text-yellow-500 '>
                         <AiOutlineArrowLeft onClick={()=>{
                             updateIndex(activeIndex - 1)
                         }} size={30} className=''/>
                     </div>
 
-                    <div className='flex'>
+                    <div className='w-full flex items-center justify-center'>
                         {
-                            items.map((item,index) => (
-                                <div key={index} className={`text-xl transition-all duration-300 cursor-pointer ${index === activeIndex ? 'text-yellow-500' : 'text-black'} `} onClick={() => updateIndex(index)} >
-                                    <RxDotFilled size={25}/>
+                            testimonials.map((item,index) => (
+                                <div key={index} className={`text-xl transition-all duration-300 cursor-pointer ${index === activeIndex ? 'text-yellow-500' : 'text-yellow-900/10'} `} onClick={() => updateIndex(index)} >
+                                    <RxDotFilled size={35}/>
                                 </div>
                             ))
                         }
                     </div>
 
-                    <div className='block cursor-pointer px-8 py-2 border-2 transition-all duration-300'>
+                    <div className='hidden cursor-pointer px-8 py-1 border-[1px] border-black transition-all duration-300 md:block hover:border-yellow-500 hover:text-yellow-500'>
                         <AiOutlineArrowRight onClick={()=>{
                             updateIndex(activeIndex + 1)
                         }} size={30} />
--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,8 +1,8 @@
 import dots1 from "./dots (1).png"
 import dots2 from "./dots (2).png"
-import slide1 from "./joker.png"
-import slide2 from "./anyrgb.com.png"
-import slide3 from "./logoOmmigner.png"
+import slide1 from "./people-1.jpg"
+import slide2 from "./people-2.jpg"
+import slide3 from "./people-3.jpg"
 
 export {
     dots1,
